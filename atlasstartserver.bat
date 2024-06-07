de7a7a61e56535e4ed510ec3927a2702c5ac7e7d
KimataDev <153996229+KimataDev@users.noreply.github.com>
2024-06-07 18:43:22 +0200

    Update atlasstartserver.bat

--- atlasstartserver.bat
+++ atlasstartserver.bat
@@ -5,11 +5,11 @@ if "%~1" == "" (
     exit /b 1
 )
 
+echo Starting Server...
+
 cd AtlasTools\RedisDatabase
-echo Starting Redis Server...
 start /b redis-server.exe redis.conf
 
 cd ..\..\
 cd ShooterGame\Binaries\Win64
-echo Starting ATLAS Dedicated Server...
 start /b /wait /high ShooterGameServer.exe %*
