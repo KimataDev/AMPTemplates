443c6dfb6b07115a4eed1fa94f4517eaf5f11316
KimataDev <153996229+KimataDev@users.noreply.github.com>
2024-06-08 02:23:54 +0200

    Update files

--- atlasstartserver.bat
+++ atlasstartserver.bat
@@ -13,3 +13,18 @@ start /b redis-server.exe redis.conf
 cd ..\..\
 cd ShooterGame\Binaries\Win64
 start /b /wait /high ShooterGameServer.exe %*
+
+:inputLoop
+set /p input=
+
+if /i "%input%"=="stop" (
+    echo Stopping Server...
+	taskkill /f /im redis-server.exe
+    taskkill /f /im ShooterGameServer.exe
+    exit /b
+    exit /b
+) else (
+    echo Undefined command: %input%
+)
+
+goto inputLoop
