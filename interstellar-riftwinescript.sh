85fa9f0fb342c13998b4a748e95e9d83ba7bce2c
KimataDev <153996229+KimataDev@users.noreply.github.com>
2024-06-12 23:44:54 +0200

    Update files

--- interstellar-riftwinescript.sh
+++ interstellar-riftwinescript.sh
@@ -23,6 +23,7 @@ chmod +x winetricks
 ./winetricks corefonts > winescript_log.txt 2>&1
 ./winetricks sound=disabled >> winescript_log.txt 2>&1
 ./winetricks -q vcrun2012 >> winescript_log.txt 2>&1
+./winetricks -q vcrun2019 >> winescript_log.txt 2>&1
 ./winetricks -q dotnet472 >> winescript_log.txt 2>&1
 rm -rf ~/.cache/winetricks ~/.cache/fontconfig
 
