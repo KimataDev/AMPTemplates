a4df765f2552bc9f0a878e9c2c02ffb2d29d23de
KimataDev <153996229+KimataDev@users.noreply.github.com>
2024-06-12 02:20:54 +0200

    Update interstellar-riftwinescript.sh

--- interstellar-riftwinescript.sh
+++ interstellar-riftwinescript.sh
@@ -14,14 +14,12 @@ rm display.log
 
 wget -q -N https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
 chmod +x winetricks
-wget -q -O $WINEPREFIX/mono.msi https://dl.winehq.org/wine/wine-mono/8.0.0/wine-mono-8.0.0-x86.msi
 
-/usr/bin/wine msiexec /i $WINEPREFIX/mono.msi /qn /quiet /norestart /log $WINEPREFIX/mono_install.log
 export WINEPREFIX="$SCRIPTDIR/interstellar-rift/.wine"
 export WINEARCH=win64
 export WINEDEBUG=-all
 export WINEDLLOVERRIDES="mscoree=d;mshtml="
-export DISPLAY=:$DPY_NUM
+export DISPLAY=:0
 ./winetricks corefonts > winescript_log.txt 2>&1
 ./winetricks sound=disabled >> winescript_log.txt 2>&1
 ./winetricks -q vcrun2012 >> winescript_log.txt 2>&1
